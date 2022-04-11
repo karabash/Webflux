e072e88cf44d641099e13471ffefc397bb8293c4
karabash <whyewre@gmail.com>
2022-04-11 22:14:51 +0200

    added som tests in FluxTest

--- src/test/java/FluxTest.java
+++ src/test/java/FluxTest.java
@@ -1,2 +1,67 @@
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
 public class FluxTest {
+    private static final List<String> LIST_OF_VALUES = Arrays.asList("A", "B", "C");
+    private static final Stream INT_STREAM = Stream.of(1, 2, 3, 4);
+
+    /**
+     * Creating a Flux with values.
+     */
+    @Test
+    void firstFlux() {
+        Flux.just("A", "B", "C")
+                .log()
+                .subscribe();
+    }
+
+    /**
+     * Creating flux from list.
+     */
+    @Test
+    void fluxFromIterable() {
+        Flux.just(Arrays.asList("A", "B", "C"))
+                .log()
+                .subscribe();
+        /* obs all values will be passed to the subscriber (onNext) as:
+        onNext[A,B,C]
+         */
+    }
+
+    /**
+     * Creating flux from list and iterating values
+     */
+    @Test
+    void fluxFromItarable() {
+        Flux.fromIterable(FluxTest.LIST_OF_VALUES)
+                .log()
+                .subscribe();
+    }
+
+    /**
+     * creating flux from stream
+     */
+    @Test
+    void fluxFromIntStream() {
+        Flux.fromStream(FluxTest.INT_STREAM)
+                .log()
+                .subscribe();
+    }
+
+    /**
+     * creating type of stream suppler
+     */
+    @Test
+    void fluxFromIntStream1() {
+        Supplier<Stream<Integer>> supStream = () -> Stream.of(1, 2, 3, 5);
+        Flux.fromStream(supStream.get())
+                .log()
+                .subscribe();
+    }
+
 }
